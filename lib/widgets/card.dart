466dc0b2afbe8124f33fcb10caa944b3321685af
RowanTamer23 <rowan.tamer232001@gmail.com>
2025-07-15 19:00:39 +0300

    firebase

--- lib/widgets/card.dart
+++ lib/widgets/card.dart
@@ -1,108 +1,123 @@
-import 'package:firebase_database/firebase_database.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class CardWid extends StatelessWidget {
-  final String? itemId;
-  final String? itemName;
-  final int? calories;
-  final double? price;
-  final String? imageUrl;
+  final String itemId;
+  final String collectionName;
 
   const CardWid({
     super.key,
-    this.itemId,
-    this.itemName,
-    this.calories,
-    this.price,
-    this.imageUrl,
+    required this.itemId,
+    required this.collectionName,
   });
 
   @override
   Widget build(BuildContext context) {
-    final databaseRef = FirebaseDatabase.instance.ref();
-    databaseRef.once().then((DatabaseEvent event) {
-      // Use event.snapshot.value
-    });
+    return StreamBuilder<DocumentSnapshot>(
+      stream: FirebaseFirestore.instance
+          .collection(collectionName)
+          .doc(itemId)
+          .snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator());
+        }
+        if (!snapshot.hasData || !snapshot.data!.exists) {
+          return Center(child: Text('No data found'));
+        }
 
-    return Container(
-      margin: EdgeInsets.only(right: 16),
-      padding: EdgeInsets.all(16),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(8),
-        border: Border.all(color: Color(0xffEAECF0)),
-      ),
-      width: 200,
-      height: 150,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          if (imageUrl != null)
-            Container(
-              height: 80,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(4),
-                image: DecorationImage(
-                  image: NetworkImage(imageUrl!),
-                  fit: BoxFit.cover,
-                ),
-              ),
-            ),
+        final data = snapshot.data!.data() as Map<String, dynamic>;
+        final imageUrl = data['image'] as String?;
+        final foodName = data['food_name'] as String?;
+        final calories = data['calories']?.toDouble() ?? 0.0;
+        final price = data['price']?.toDouble() ?? 0.0;
 
-          SizedBox(height: 8),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                itemName ?? 'Loading...',
-                style: TextStyle(
-                  fontFamily: 'Poppins',
-                  fontWeight: FontWeight.w600,
-                  fontSize: 14,
-                ),
-              ),
-
-              Text(
-                '${calories ?? 0} cal',
-                style: TextStyle(
-                  fontFamily: 'Poppins',
-                  fontSize: 12,
-                  color: Color(0xff959595),
-                ),
-              ),
-            ],
+        return Container(
+          margin: EdgeInsets.only(right: 16),
+          padding: EdgeInsets.all(16),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(8),
+            border: Border.all(color: Color(0xffEAECF0)),
           ),
-          Spacer(),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          width: 200,
+          height: 180,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text(
-                '\$${price?.toStringAsFixed(2) ?? '0.00'}',
-                style: TextStyle(
-                  fontFamily: 'Poppins',
-                  fontSize: 12,
-                  color: Color(0xffF25700),
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-              TextButton(
-                onPressed: () {
-                  // Add to cart functionality
-                },
-                child: Text(
-                  'Add',
-                  style: TextStyle(
-                    color: Color(0xffF25700),
-                    fontFamily: 'Poppins',
-                    fontWeight: FontWeight.w500,
+              if (imageUrl != null)
+                Container(
+                  height: 70,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(4),
+                    image: DecorationImage(
+                      image: NetworkImage(imageUrl),
+                      fit: BoxFit.cover,
+                    ),
                   ),
                 ),
+              SizedBox(height: 30),
+              Container(
+                height: 20,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      foodName ?? 'Loading...',
+                      style: TextStyle(
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.w600,
+                        fontSize: 14,
+                      ),
+                    ),
+                    Text(
+                      '${calories.toStringAsFixed(0)} cal',
+                      style: TextStyle(
+                        fontFamily: 'Poppins',
+                        fontSize: 12,
+                        color: Color(0xff959595),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Spacer(),
+              Container(
+                height: 35,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      '\$${price.toStringAsFixed(2)}',
+                      style: TextStyle(
+                        fontFamily: 'Poppins',
+                        fontSize: 12,
+                        color: Color(0xffF25700),
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        // Add to cart functionality
+                      },
+                      child: Text(
+                        'Add',
+                        style: TextStyle(
+                          color: Color(0xffF25700),
+                          fontFamily: 'Poppins',
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
